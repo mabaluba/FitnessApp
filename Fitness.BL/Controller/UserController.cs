6f298e5f391d2b5e984e2085ad2d336b04f84c18
Max <yashanty@gmail.com>
2021-10-21 01:59:22 +0300

    done with UserController - without refactoring

--- Fitness.BL/Controller/UserController.cs
+++ Fitness.BL/Controller/UserController.cs
@@ -9,13 +9,26 @@ using System.Threading.Tasks;
 
 namespace Fitness.BL.Controller
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class UserController
     {
-        public User User { get;}
+        /// <summary>
+        /// Application user
+        /// </summary>
+        public User User { get; }
+        /// <summary>
+        /// Create new Controller
+        /// </summary>
+        /// <param name="user"></param>
         public UserController(User user)
         {
             User = user ?? throw new ArgumentNullException(nameof(user), "User cannot be NULL.");
         }
+        /// <summary>
+        /// Save user data
+        /// </summary>
         public void Save()
         {
             var formatter = new BinaryFormatter();
@@ -23,7 +36,19 @@ namespace Fitness.BL.Controller
             {
                 formatter.Serialize(fileStream, User);
             }
-            
         }
+        /// <summary>
+        /// Get user data
+        /// </summary>
+        /// <returns>Application user</returns>
+        public User Load()
+        {
+            var formatter = new BinaryFormatter();
+            using (var fileStream = new FileStream("users.dat", FileMode.Open))
+            {
+                return formatter.Deserialize(fileStream) as User;
+            }
+        }
+
     }
 }
